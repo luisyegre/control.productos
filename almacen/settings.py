318fa40d8a92ba7fe5487777f1826513917e3d6e
luis yegre <luis_yegre@email.com>
2021-05-07 22:20:46 -0400

    statics files root

--- almacen/settings.py
+++ almacen/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 AUTH_USER_MODEL='user.User'
 
@@ -131,4 +131,4 @@ LOGOUT_REDIRECT_URL='/auth/login'
 LOGIN_URL='/auth/login'
  
 MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR / 'media'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
